ca7cafe95f59abbe80988dcb794bb48c64929384
Leader <82439819+malo-dev@users.noreply.github.com>
2025-07-06 16:58:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.get('/api/properties', async (req, res) => {
   try {
